01ba313895e830c672bc6215bd8d254d05b07014
phgnm <phuongnam710648@gmail.com>
2024-08-05 20:34:37 +0700

    initialization

---
+++ source/graphic.py
@@ -0,0 +1,11 @@
+import sys
+class graphics:
+    def __init__(self):
+        self.map = None
+        self.agent = None
+        self.gold = None
+        self.wumpus = None
+        self.pit = None
+        self.arrow = None
+    def run(self):
+        print("Chua co gi dau, hihi")
---
+++ source/main.py
@@ -0,0 +1,4 @@
+from graphic import *
+
+app = graphics()
+app.run()
